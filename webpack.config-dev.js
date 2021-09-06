13ac69f0834c3eb0ff6d4fead421eae54f162a1c
Cromeror <cristobalromerorossi@gmail.com>
2021-09-06 17:41:55 -0500

    chore(build): add transpiler for React component in customs Js

--- webpack.config-dev.js
+++ webpack.config-dev.js
@@ -2,12 +2,29 @@
 const path = require('path');
 
 // eslint-disable-next-line no-undef
-module.exports = {
-  entry: './src/customJs/index.js',
-  mode: 'development',
-  output: {
-    filename: 'index.js',
-    // eslint-disable-next-line no-undef
-    path: path.resolve(__dirname, 'public/customJs'),
-  },
+module.exports = function () {
+  // Do this as the first thing so that any code reading it knows the right env.
+  // eslint-disable-next-line no-undef
+  process.env.BABEL_ENV = 'development';
+  // eslint-disable-next-line no-undef
+  process.env.NODE_ENV = 'development';
+
+  return {
+    entry: './src/customJs/index.js',
+    mode: 'development',
+    output: {
+      filename: 'index.js',
+      // eslint-disable-next-line no-undef
+      path: path.resolve(__dirname, 'public/customJs'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+      ],
+    },
+  };
 };
