5a652d244ac859914bed0572319719a5c4ba55d5
Andrés Moschini <andresmoschini@gmail.com>
2022-05-23 16:39:43 -0300

    fix: remove test tools from editor initialization

--- src/customJs/index.js
+++ src/customJs/index.js
@@ -1,16 +1,9 @@
-import subscribeTool from './subscribeTool';
-import { getTwitterPropertyConfig } from './socialTool/TwitterProperty';
-import { getLinkedInPropertyConfig } from './socialTool/LinkedInProperty';
-import { getFacebookPropertyConfig } from './socialTool/FacebookProperty';
-import { getSocialToolConfig } from './socialTool/SocialTool';
 import { getSocialShareToolConfig } from './socialShareTool';
-import { setCompanyTitle } from './sharedSingletonModule';
-import { getCompanyTitleDemoConfig } from './companyTitleDemo';
 import { setLocale } from './localization';
 
 const unlayer = window.unlayer;
 
-const { locale, companyTitle } = window['unlayer-extensions-configuration'];
+const { locale } = window['unlayer-extensions-configuration'];
 
 const unlayerLocales = {
   es: 'es-ES',
@@ -19,14 +12,6 @@ const unlayerLocales = {
 const unlayerLocale = unlayerLocales[locale] ?? unlayerLocales['es'];
 
 setLocale(unlayerLocale);
-setCompanyTitle(companyTitle);
-unlayer.registerPropertyEditor(getTwitterPropertyConfig());
-unlayer.registerPropertyEditor(getLinkedInPropertyConfig());
-unlayer.registerPropertyEditor(getFacebookPropertyConfig());
-unlayer.registerTool(getSocialToolConfig());
-unlayer.registerTool(getCompanyTitleDemoConfig());
+
 // Register Properties and tool Social Share Tool
 unlayer.registerTool(getSocialShareToolConfig());
-
-// TODO: Verify to delete this
-export default { subscribeTool };
