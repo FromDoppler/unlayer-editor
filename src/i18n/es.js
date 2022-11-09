48a6d603c5052fb20d27c3f940708d2677f96ff0
Cromeror <cristobalromerorossi@gmail.com>
2022-11-09 18:33:56 -0500

    test: update translations

--- src/i18n/es.js
+++ src/i18n/es.js
@@ -1,19 +1,19 @@
 export const messages_es = {
   alignment: `Alineación`,
   big: `Grande`,
+  configuration: 'Configuración',
   medium: `Normal`,
+  pay_button_link: 'Solicitud de pago detallada',
+  payu_large_button:
+    'https://cdn.fromdoppler.com/unlayer-editor/assets/payu-button-large-es.png',
+  payu_medium_button:
+    'https://cdn.fromdoppler.com/unlayer-editor/assets/payu-button-medium-es.png',
+  payu_small_button:
+    'https://cdn.fromdoppler.com/unlayer-editor/assets/payu-button-small-es.png',
   size: `Tamaño`,
   small: `Pequeño`,
   social_networks: `Redes sociales`,
   'tools.tabs.body': `Estilos generales`,
   'tools.tabs.images': `Imágenes de stock`,
   'tools.tabs.uploads': `Galería`,
-  pay_button_link: 'Solicitud de pago detallada',
-  configuration: 'Configuración',
-  payu_small_button:
-    'https://cdn.fromdoppler.com/unlayer-editor/assets/payu-button-small-es.png',
-  payu_medium_button:
-    'https://cdn.fromdoppler.com/unlayer-editor/assets/payu-button-medium-es.png',
-  payu_large_button:
-    'https://cdn.fromdoppler.com/unlayer-editor/assets/payu-button-large-es.png',
 };
