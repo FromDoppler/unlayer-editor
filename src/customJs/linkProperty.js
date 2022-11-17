c2d40b35bf1a66ea879ed3e268321335eafa52a2
Cromeror <cristobalromerorossi@gmail.com>
2022-11-17 13:13:59 -0500

    feat: add help text

--- src/customJs/linkProperty.js
+++ src/customJs/linkProperty.js
@@ -13,19 +13,23 @@ const customPropertyLabel = (label) => `
 const linkPropertyWidget = {
   render(value, updateValue, data) {
     const label = data && data.label ? customPropertyLabel(data.label) : '';
+    const help = data && data.help ? data.help : '';
 
     return `
             ${label}
-            <div class="href_field input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text">
-                  ${intl.formatMessage({ id: 'editor.link.url' })}
-                </span>
+            <div class="blockbuilder-widget-label mb-2">
+              <div class="href_field input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    ${intl.formatMessage({ id: 'editor.link.url' })}
+                  </span>
+                </div>
+                <input type="url" class="form-control" value="${
+                  value ? value : ''
+                }">
               </div>
-              <input type="url" class="form-control" value="${
-                value ? value : ''
-              }">
             </div>
+            ${help}
       `;
   },
   mount(node, value, updateValue, data) {
