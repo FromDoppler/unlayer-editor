d2383f44e8ae5844a2c0f35d9f707e44a680aeb2
cromeror <cromeror@makingsense.com>
2022-12-22 16:12:01 -0500

    feat: social share property is selectable for accessibility

--- src/customJs/socialShareTool/enableSocialProperty.js
+++ src/customJs/socialShareTool/enableSocialProperty.js
@@ -3,8 +3,9 @@ const React = window.unlayer.React;
 
 const Toggle = ({ label, onChange }) => {
   const [active, setActive] = React.useState(true);
+  const [focus, setFocus] = React.useState(false);
   const clickEvent = (e) => {
-    e.stopPropagation();
+    e.preventDefault();
     setActive((prev) => {
       if (onChange) {
         onChange(!prev);
@@ -23,9 +24,12 @@ const Toggle = ({ label, onChange }) => {
       </label>
       <label className="blockbuilder-label-right">
         <div
-          className={`react-toggle ${active ? 'react-toggle--checked' : ''}`}
+          className={`react-toggle ${active ? 'react-toggle--checked' : ''} ${
+            focus ? 'react-toggle--focus' : ''
+          }`}
+          onClick={clickEvent}
         >
-          <div className="react-toggle-track" onClick={clickEvent}>
+          <div className="react-toggle-track">
             <div className="react-toggle-track-check">
               <svg width="14" height="11" viewBox="0 0 14 11">
                 <path
@@ -45,7 +49,13 @@ const Toggle = ({ label, onChange }) => {
               </svg>
             </div>
           </div>
-          <div className="react-toggle-thumb" onClick={clickEvent}></div>
+          <div className="react-toggle-thumb"></div>
+          <input
+            className="react-toggle-screenreader-only"
+            type="checkbox"
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+          />
         </div>
       </label>
     </div>
