605afe7a42a826dac8b83fb1f0738278f52a3276
Andres Moschini <andresmoschini@gmail.com>
2023-03-10 14:32:24 -0300

    chore: quotes

--- src/customJs/payuButtonTool/PayuButtonTool.js
+++ src/customJs/payuButtonTool/PayuButtonTool.js
@@ -5,12 +5,12 @@ export const PayuButtonTool = unlayer.createViewer({
       size === 'small' ? 116 : size === 'medium' ? 145 : 182;
 
     return `
-      <div align=${alignment} role="container">
-        <a role="link" href=${paymentURL ? paymentURL : '#'} target="_blank">
+      <div align=${alignment} role='container'>
+        <a role='link' href=${paymentURL ? paymentURL : '#'} target='_blank'>
           <img
             style='width: ${buttonImgWidth}'
-            src="payu_${size}_button"
-            alt="payu_button"
+            src='payu_${size}_button'
+            alt='payu_button'
           />
         </a>
       </div>
