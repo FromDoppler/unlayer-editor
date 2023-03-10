12a688124936c2ea1ab3f5c38dcb7e4be8549e14
Andres Moschini <andresmoschini@gmail.com>
2023-03-10 14:01:15 -0300

    chore: step 2

--- src/customJs/payuButtonTool/index.js
+++ src/customJs/payuButtonTool/index.js
@@ -6,7 +6,7 @@ const React = window.unlayer.React;
 export const getPayuButtonToolConfig = () => ({
   name: 'payu_button_tool',
   label: 'payu_button',
-  icon: process.env.PUBLIC_URL + '/assets/payu_button.svg',
+  icon: 'fa-smile',
   supportedDisplayModes: ['web', 'email'],
   category: 'contents',
   type: 'whatever',
@@ -23,26 +23,6 @@ export const getPayuButtonToolConfig = () => ({
           widget: 'link_property',
           data: {
             label: 'pay_button_link',
-            help: (
-              <div
-                style={{
-                  fontStyle: 'italic',
-                  fontSize: '13px',
-                  lineHeight: '1.6',
-                  color: '#525252',
-                  fontFamily: 'Roboto,sans-serif',
-                }}
-              >
-                {'payu_help'}
-                <a
-                  target="_blank"
-                  style={{ color: '#33AC72', textDecoration: 'none' }}
-                  href={'payu_help_link'}
-                >
-                  &nbsp;{'help'}
-                </a>
-              </div>
-            ),
           },
         },
         size: {
@@ -81,18 +61,5 @@ export const getPayuButtonToolConfig = () => ({
         // );
       },
     },
-  },
-  validator: ({ defaultErrors, values }) => {
-    if (!values.paymentURL) {
-      defaultErrors.push({
-        id: 'CUSTOM_ERROR',
-        icon: process.env.PUBLIC_URL + '/assets/payu_button.svg',
-        severity: 'WARNING',
-        title: 'tabs.audit.rules.payu_button.empty_links.title',
-        description: 'tabs.audit.rules.payu_button.empty_links.description',
-      });
-    }
-
-    return defaultErrors;
   },
 });
