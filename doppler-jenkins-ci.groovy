fa311a3ef80f3af891b32cadd632542ab5617a0b
Andres Moschini <andresmoschini@gmail.com>
2023-04-14 15:16:31 -0300

    build: remove symbolic link for Jenkinsfile

--- doppler-jenkins-ci.groovy
+++ doppler-jenkins-ci.groovy
@@ -1,1 +1,119 @@
-Jenkinsfile
+pipeline {
+  agent any
+  stages {
+    stage('Verify git commit conventions') {
+      steps {
+        sh 'sh ./gitlint.sh'
+      }
+    }
+    stage('Verify .sh files') {
+      steps {
+        sh 'docker build --target verify-sh .'
+      }
+    }
+    stage('Restore') {
+      steps {
+        sh 'docker build --target restore .'
+      }
+    }
+    stage('Verify Format') {
+      steps {
+        sh 'docker build --target verify-format .'
+      }
+    }
+    stage('Test') {
+      steps {
+        sh 'docker build --target test .'
+      }
+    }
+    stage('Build') {
+      steps {
+        sh 'docker build --target build .'
+      }
+    }
+    stage('Publish in CDN') {
+      environment {
+          PKG_NAME = "unlayer-editor"
+      }
+      stages {
+        // TODO: Inject CDN SFTP credentials here
+        stage('Publish pre-release images from pull request') {
+          when {
+            changeRequest target: 'main'
+          }
+          steps {
+            sh '''
+              sh ./build-n-publish.sh \
+                --package=${PKG_NAME} \
+                --commit=${GIT_COMMIT} \
+                --name=pr-${CHANGE_ID}
+            '''
+          }
+        }
+        stage('Publish pre-release images from main') {
+          when {
+            branch 'main'
+          }
+          steps {
+            sh '''
+              sh build-n-publish.sh \
+                --package=${PKG_NAME} \
+                --commit=${GIT_COMMIT} \
+                --name=main
+            '''
+          }
+        }
+        stage('Publish pre-release images from INT') {
+          when {
+            branch 'INT'
+          }
+          steps {
+            sh '''
+              sh build-n-publish.sh \
+                --package=${PKG_NAME} \
+                --commit=${GIT_COMMIT} \
+                --name=INT
+            '''
+          }
+        }
+        stage('Publish final version images') {
+          when {
+            expression {
+              return isVersionTag(readCurrentTag())
+            }
+          }
+          steps {
+            sh '''
+              sh build-n-publish.sh \
+                --package=${PKG_NAME} \
+                --commit=${GIT_COMMIT} \
+                --version=${TAG_NAME}
+            '''
+          }
+        }
+      }
+    }
+  }
+}
+
+def boolean isVersionTag(String tag) {
+  echo "checking version tag $tag"
+
+  if (tag == null) {
+    return false
+  }
+
+  // use your preferred pattern
+  def tagMatcher = tag =~ /v\d+\.\d+\.\d+/
+
+  return tagMatcher.matches()
+}
+
+def CHANGE_ID = env.CHANGE_ID
+
+// https://stackoverflow.com/questions/56030364/buildingtag-always-returns-false
+// workaround https://issues.jenkins-ci.org/browse/JENKINS-55987
+// TODO: read this value from Jenkins provided metadata
+def String readCurrentTag() {
+  return sh(returnStdout: true, script: 'echo ${TAG_NAME}').trim()
+}
