45a8ce3d8819ae818c26a694acf17cc3d9827a47
Andres Moschini <andresmoschini@gmail.com>
2023-07-03 12:30:06 -0300

    chore: show a console log message when promotionCodeEnabled

--- src/customJs/index.js
+++ src/customJs/index.js
@@ -8,7 +8,7 @@ import { getConfiguration } from './configuration';
 
 const unlayer = window.unlayer;
 
-const { locale } = getConfiguration();
+const { locale, promotionCodeEnabled } = getConfiguration();
 
 const unlayerLocales = {
   es: 'es-ES',
@@ -30,3 +30,11 @@ unlayer.registerPropertyEditor(getLinkPropertyConfiguration());
 unlayer.registerPropertyEditor(getEnableSocialPropertyConfig());
 unlayer.registerTool(getSocialShareToolConfig());
 unlayer.registerTool(getPayuButtonToolConfig());
+
+if (promotionCodeEnabled) {
+  console.log('promotionCodeEnabled, it will show the Promo Code Tool soon...');
+  // TODO: do something like this
+  // unlayer.registerTool(getPromoCodeToolConfig());
+  // For Promo Code Tool, promotionCodeEnabled is enough to decide to show or
+  // hide the tool. Other tools depend on the campaign.
+}
