0d35575d45b836c2bdfc7d5114a021726d670b36
leoslopez <llopez@makingsense.com>
2023-08-02 15:41:52 -0300

    feat: treat the obtained promo code items

--- src/customJs/properties/promo_codes/types.ts
+++ src/customJs/properties/promo_codes/types.ts
@@ -6,3 +6,13 @@ export type StoresValue = string | EMPTY_SELECTION;
 export type StoreDependentToolValues = {
   store: StoresValue;
 };
+
+export type PromoCodeItem = {
+  code: string;
+  type: 'percent' | 'money';
+  value: number;
+  startDate: Date | undefined; // TODO: verify type
+  endDate: Date | undefined; // TODO: verify type
+  useLimit: number;
+  minPaymentAmount: number;
+};
