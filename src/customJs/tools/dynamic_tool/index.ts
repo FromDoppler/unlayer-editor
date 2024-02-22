fdaab991da5876da2aacd1dcd4eb45461485e3c9
cketibian <cketibian@makingsense.com>
2024-02-22 17:28:43 -0300

    refactor: move all posible property options out

--- src/customJs/tools/dynamic_tool/index.ts
+++ src/customJs/tools/dynamic_tool/index.ts
@@ -1,6 +1,12 @@
 import { $t } from '../../localization';
 import { ProductViewer } from './ProductViewer';
-import { ProductToolDefinition, ProductLayout, ItemsStructure } from './types';
+import {
+  ProductToolDefinition,
+  ProductLayout,
+  ItemsStructure,
+  OptionTool,
+  ProductPropertyGroups,
+} from './types';
 import { ASSETS_BASE_URL, DYNAMIC_TOOL_TYPE } from '../../constants';
 import {
   autoWidthProperty,
@@ -59,76 +65,94 @@ export const getDynamicToolDefinition: (
         options: {
           structure: itemStructuretProperty(),
         },
-      },
-      layout: {
-        title: $t('_dp.layout'),
-        options: {
-          layout: productLayoutProperty(),
-          backgroundColor: colorProperty({
-            label: $t('editor.background_color.label'),
-          }),
-        },
-      },
-      image: {
-        title: $t('editor.image.label'),
-        options: {
-          imageShown: toggleShowProperty(),
-          imageAutoWith: autoWidthProperty(),
-        },
-      },
-      title: {
-        title: $t('_dp.title'),
-        options: {
-          titleShown: toggleShowProperty(),
-          titleFont: fontFamilyProperty(),
-          titleFontWeight: fontWeightProperty({
-            defaultValue: 700,
-          }),
-          titleFontSize: fontSizeProperty({
-            defaultValue: DEFAULT_FONT_SIZE,
-          }),
-          titleColor: colorProperty(),
-        },
-      },
-      price: {
-        title: $t('_dp.price'),
-        options: {
-          priceShown: toggleShowProperty(),
-          priceFont: fontFamilyProperty(),
-          priceFontWeight: fontWeightProperty(),
-          priceFontSize: fontSizeProperty({
-            label: $t('editor.font_size.label'),
-            defaultValue: DEFAULT_FONT_SIZE,
-          }),
-          priceColor: colorProperty({
-            label: $t('editor.text_color.label'),
-            defaultValue: DEFAULT_GREEN_COLOR,
-          }),
-        },
-      },
-
-      button: {
-        title: $t('_dp.product_button'),
-        options: {
-          buttonShown: toggleShowProperty(),
-          buttonText: textProperty({
-            label: $t('_dp.product_button_text'),
-            defaultValue: $t('_dp.product_button_default_value'),
-          }),
-          buttonFont: fontFamilyProperty(),
-          buttonFontWeight: fontWeightProperty({
-            defaultValue: 700,
-          }),
-          buttonFontSize: fontSizeProperty({
-            defaultValue: '15px',
-          }),
-          buttonColors: buttonColorsProperty(),
-          buttonAutoWith: autoWidthProperty(),
-          buttonBorder: borderProperty(),
-          buttonBorderRadius: borderRadiusProperty(),
-        },
+  const options: ProductPropertyGroups = {
+    product: {
+      title: $t('_dp.cart_item_structure'),
+      options: {
+        structure: itemStructuretProperty(),
       },
     },
+    layout: {
+      title: $t('_dp.layout'),
+      options: {
+        layout: productLayoutProperty(),
+        backgroundColor: colorProperty({
+          label: $t('editor.background_color.label'),
+        }),
+      },
+    },
+    image: {
+      title: $t('editor.image.label'),
+      options: {
+        imageShown: toggleShowProperty(),
+        imageAutoWith: autoWidthProperty(),
+      },
+    },
+    title: {
+      title: $t('_dp.title'),
+      options: {
+        titleShown: toggleShowProperty(),
+        titleFont: fontFamilyProperty(),
+        titleFontWeight: fontWeightProperty({
+          defaultValue: 700,
+        }),
+        titleFontSize: fontSizeProperty({
+          defaultValue: DEFAULT_FONT_SIZE,
+        }),
+        titleColor: colorProperty(),
+      },
+    },
+    quantity: {
+      title: $t('_dp.quantity'),
+      options: {
+        quantityShown: toggleShowProperty(),
+        quantityFont: fontFamilyProperty(),
+        quantityFontWeight: fontWeightProperty(),
+        quantityFontSize: fontSizeProperty({
+          label: $t('editor.font_size.label'),
+          defaultValue: '18px',
+        }),
+        quantityColor: colorProperty(),
+      },
+    },
+    price: {
+      title: $t('_dp.price'),
+      options: {
+        priceShown: toggleShowProperty(),
+        priceFont: fontFamilyProperty(),
+        priceFontWeight: fontWeightProperty(),
+        priceFontSize: fontSizeProperty({
+          label: $t('editor.font_size.label'),
+          defaultValue: DEFAULT_FONT_SIZE,
+        }),
+        priceColor: colorProperty({
+          label: $t('editor.text_color.label'),
+          defaultValue: DEFAULT_GREEN_COLOR,
+        }),
+      },
+    },
+    button: {
+      title: $t('_dp.product_button'),
+      options: {
+        buttonShown: toggleShowProperty(),
+        buttonText: textProperty({
+          label: $t('_dp.product_button_text'),
+          defaultValue: $t('_dp.product_button_default_value'),
+        }),
+        buttonFont: fontFamilyProperty(),
+        buttonFontWeight: fontWeightProperty({
+          defaultValue: 700,
+        }),
+        buttonFontSize: fontSizeProperty({
+          defaultValue: '15px',
+        }),
+        buttonColors: buttonColorsProperty(),
+        buttonAutoWith: autoWidthProperty(),
+        buttonBorder: borderProperty(),
+        buttonBorderRadius: borderRadiusProperty(),
+      },
+    },
+  };
     createDynamicContet(htmlComponent: string, values: any) {
       const htmlDinamicComponent = htmlComponent
         .replace(
