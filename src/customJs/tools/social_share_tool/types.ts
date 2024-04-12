0614e41492b4972292611061eef3ddf7f12848dc
cketibian <cketibian@makingsense.com>
2024-04-12 15:01:06 -0300

    refactor: change widget by individual Toggle

--- src/customJs/tools/social_share_tool/types.ts
+++ src/customJs/tools/social_share_tool/types.ts
@@ -10,7 +10,11 @@ export type SocialShareBase = {
     social_share_size: 'big' | 'small';
   };
   social_share_network: {
-    social_share_available: SocialNetworkId[];
+    facebook_shared_property: boolean;
+    linkedin_shared_property: boolean;
+    twitter_shared_property: boolean;
+    pinterest_shared_property: boolean;
+    whatsapp_shared_property: boolean;
   };
   social_share_align: {
     social_share_align_option: Alignment;
