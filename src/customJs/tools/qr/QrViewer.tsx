66278f06fc6aae2703b3822d991282e347674aeb
cketibian <cketibian@makingsense.com>
2024-06-27 16:10:56 -0300

    fix: get url image from data uri

--- src/customJs/tools/qr/QrViewer.tsx
+++ src/customJs/tools/qr/QrViewer.tsx
@@ -1,10 +1,18 @@
-import { React } from '../../unlayer-react';
+import { React, useEffect, useState } from '../../unlayer-react';
 import { ViewerComponent } from '../../types';
 import { QrValues } from './types';
 import QRious from 'qrious';
 import { EmptyViewer } from '../../components/EmptyViewer';
+import { $t } from '../../localization';
+import { requestDopplerApp } from '../../utils/dopplerAppBridge';
 
 export const QrViewer: ViewerComponent<QrValues> = ({ values, ...rest }) => {
+  const loadingStyle = {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+  } as const;
+
   const containerStyle = {
     display: 'block',
     backgroundColor: values.backgroundColor,
@@ -12,12 +20,23 @@ export const QrViewer: ViewerComponent<QrValues> = ({ values, ...rest }) => {
     width: '100%',
   } as const;
 
-  const imageData: string = getQrImage(values);
   const formatStyle = {
     textAlign: values.qrAlignment,
   } as const;
 
+  const { loading, qrUrlImage } = useQrImage({ values });
   const isQrEmpty = values.qrValue === undefined || values.qrValue === '';
+
+  if (loading) {
+    return (
+      <div style={loadingStyle}>
+        <div className="spinner-border text-secondary" role="status"></div>
+        <span className="visually-hidden" style={containerStyle}>
+          {$t('labels.loading')}...
+        </span>
+      </div>
+    );
+  }
 
   return isQrEmpty ? (
     <EmptyViewer {...rest} />
@@ -26,7 +45,7 @@ export const QrViewer: ViewerComponent<QrValues> = ({ values, ...rest }) => {
       <div role="container" style={containerStyle}>
         <div style={formatStyle}>
           <a role="link" href={values.qrValue} target="_blank" rel="noreferrer">
-            <img src={imageData} alt={values.qrValue} />
+            <img src={qrUrlImage} alt={values.qrValue} />
           </a>
         </div>
       </div>
@@ -42,4 +61,51 @@ const getQrImage = (values: QrValues): string => {
     size: values.qrSize,
     level: 'H', // Error correction level of the QR code (L, M, Q, H)
   }).toDataURL('image/jpeg');
+};
+
+const getComponentName = (values: QrValues): string => {
+  const componetData = values as any;
+  return componetData._meta.htmlID;
+};
+
+const getImageFile = (values: QrValues) => {
+  const binary = atob(getQrImage(values).split(',')[1]),
+    array: number[] = [];
+  for (let i = 0; i < binary.length; i++) array.push(binary.charCodeAt(i));
+  const blob = new Blob([new Uint8Array(array)], { type: 'image/png' });
+  return new File([blob], `${getComponentName(values)}.jpg`, {
+    type: 'image/jpeg',
+  }) as File;
+};
+
+const useQrImage = ({ values }: { values: QrValues }) => {
+  const [loading, setLoading] = useState(false);
+  const [qrUrlImage, setqrUrlImage] = useState<string>('');
+
+  useEffect(() => {
+    if (values.qrValue === undefined || values.qrValue === '') {
+      setLoading(false);
+      setqrUrlImage('');
+      return;
+    }
+
+    setLoading(true);
+    const qrImageFile = getImageFile(values);
+
+    const { destructor } = requestDopplerApp({
+      action: 'getImageUrlFile',
+      qrImageFile,
+      callback: (value: string) => {
+        setqrUrlImage(value);
+        setLoading(false);
+      },
+    });
+
+    return destructor;
+  }, [values]);
+
+  return {
+    loading,
+    qrUrlImage,
+  };
 };
