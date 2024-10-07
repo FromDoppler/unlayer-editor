d28e449203412e1ff49031bfd4b414ef477bd004
cketibian <cketibian@makingsense.com>
2024-10-07 15:17:48 -0300

    fix: update struct group store and isDynamic flag

--- src/customJs/tools/promo_code/types.ts
+++ src/customJs/tools/promo_code/types.ts
@@ -2,8 +2,10 @@ import { EMPTY_SELECTION } from '../../constants';
 import { Alignment, PropertyGroupsFrom, ToolValuesFrom } from '../../types';
 
 export type PromoCodeBase = {
-  promo_code: {
+  promo_code_store: {
     store: string | EMPTY_SELECTION;
+    isDynamic: boolean;
+  };
   promo_code: {
     code: string | EMPTY_SELECTION;
   };
