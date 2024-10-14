c43b25b45ba35a492f7224dcf524dfbcd9b9c120
cketibian <cketibian@makingsense.com>
2024-10-14 02:40:38 -0300

    feat: add dynamic promo code widget

---
+++ src/customJs/properties/dynamic_promo_code/types.ts
@@ -0,0 +1,12 @@
+import { PromoCodeTypes } from '../../tools/promo_code/types';
+
+export type DynamicPromoCodeDependentToolValues = {
+  type: PromoCodeTypes;
+  amount: string;
+  expire_days: string;
+  min_price: string;
+  prefixe_code: string;
+  includes_shipping: boolean;
+  first_consumer_purchase: boolean;
+  combines_with_other_discounts: boolean;
+};
