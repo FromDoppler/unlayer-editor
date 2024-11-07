6abf000d6e9d7442e447e2fe2347c330520c2dea
cketibian <cketibian@makingsense.com>
2024-11-07 14:57:18 -0300

    fix: update label content

--- src/customJs/tools/dynamic_tool/propertyHelper.ts
+++ src/customJs/tools/dynamic_tool/propertyHelper.ts
@@ -40,7 +40,7 @@ export const itemStructuretProperty: () => UnlayerProperty<ItemsStructure> =
 export const recommendedStructureProperty: () => UnlayerProperty<RecommendedStructure> =
   () =>
     dropdownProperty({
-      label: undefined,
+      label: $t('_dp.recommended_structure'),
       defaultValue: '3',
       options: [
         { label: $t('_dp.recommended_structure_option_1'), value: '1' },
@@ -80,7 +80,7 @@ export const getRecommended_type_options: () => Readonly<
 export const recommendedTypeProperty: () => UnlayerProperty<RecommendedType> =
   () =>
     dropdownProperty({
-      label: undefined,
+      label: $t('_dp.recommended_type'),
       defaultValue: 'best_selling',
       options: getRecommended_type_options(),
     } as const);
