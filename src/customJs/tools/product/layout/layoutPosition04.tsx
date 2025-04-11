432f3066adba2a6cfb8c305a485f63d1f33f28e8
emoralesms <emorales@makingsense.com>
2025-04-11 16:03:37 -0300

    feat: layouts with dc type

--- src/customJs/tools/product/layout/layoutPosition04.tsx
+++ src/customJs/tools/product/layout/layoutPosition04.tsx
@@ -12,21 +12,34 @@ export const ProductLayoutViewer04 = ({ values }: { values: any }) => {
   return (
     <div>
       <section style={headerSectionStyle} data-testid="title-container">
-        <span style={values.title.style}>{values.title.value}</span>
+        <span
+          style={values.title.style}
+          dangerouslySetInnerHTML={{ __html: values.title.value }}
+          {...(values.title.isDynamic && { 'data-dc-type': 'DC:TITLE' })}
+        />
         <span style={values.prices.style} data-testid="prices-container">
-          <span style={values.prices.default.style}>
-            {values.prices.default.value}
-          </span>
-          <span style={values.prices.discount.style}>
-            {values.prices.discount.value}
-          </span>
+          <span
+            style={values.prices.default.style}
+            dangerouslySetInnerHTML={{ __html: values.prices.default.value }}
+            {...(values.prices.default.isDynamic && {
+              'data-dc-type': 'DC:PRICES_DEFAULT',
+            })}
+          />
+          <span
+            style={values.prices.discount.style}
+            dangerouslySetInnerHTML={{ __html: values.prices.discount.value }}
+            {...(values.prices.discount.isDynamic && {
+              'data-dc-type': 'DC:PRICES_DISCOUNT',
+            })}
+          />
         </span>
       </section>
       <section style={values.section.image.style} data-testid="image-container">
         <img
           style={values.image.style}
           src={values.image.value}
-          alt="product image"
+          alt="Product image"
+          {...(values.image.isDynamic && { 'data-dc-type': 'DC:IMAGE' })}
         />
       </section>
       <section
@@ -36,12 +49,21 @@ export const ProductLayoutViewer04 = ({ values }: { values: any }) => {
         <span
           style={values.description.style}
           dangerouslySetInnerHTML={{ __html: values.description.value }}
+          {...(values.description.isDynamic && {
+            'data-dc-type': 'DC:DESCRIPTION',
+          })}
         />
         <span
           style={values.info.style}
           dangerouslySetInnerHTML={{ __html: values.info.value }}
+          {...(values.info.isDynamic && { 'data-dc-type': 'DC:INFO' })}
         />
-        <span style={values.discount.style}>{values.discount.value}</span>
+        <span
+          style={values.discount.style}
+          {...(values.discount.isDynamic && { 'data-dc-type': 'DC:DISCOUNT' })}
+        >
+          {values.discount.value}
+        </span>
         <a
           style={values.button.style}
           role="link"
